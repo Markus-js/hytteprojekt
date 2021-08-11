92a030412e1a4acdad6b7441bd9ddfb07352f8c5
Markus-js <markusbackhausen@outlook.com>
2021-08-11 11:04:56 +0200

    emailjs test

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,43 +1,47 @@
 import React from "react";
-import emailjs from 'emailjs-com';
-import{ init } from 'emailjs-com';
+import emailjs from "emailjs-com";
+import { init } from "emailjs-com";
 init("user_iKlFVYLk9kyYQV2fO6bD6");
 
 export default function Form({ formData }) {
   //const priceFixed = parseInt(formData.price).toFixed(0);
 
+  function sendEmail(e) {
+    e.preventDefault();
 
- 
-  
-  
-  
-    function sendEmail(e) {
-      e.preventDefault();
-  
-      emailjs.sendForm("gmail", "template_83c911k", e.target, "user_iKlFVYLk9kyYQV2fO6bD6")
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
-  
-    return (
-      <form className="contact-form" onSubmit={sendEmail}>
-        <input type="text" name="nr" />
-        <label>Name</label>
-        <input type="text" name="name" />
-        <label>num</label>
-        <input type="number" name="number" />
-        <label>Email</label>
-        <input type="email" name="email" />
-        <label>Message</label>
-        <input type="submit" value="Send" />
-      </form>
-    );
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_b9c11vh",
+        e.target,
+        "user_iKlFVYLk9kyYQV2fO6bD6"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
   }
 
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </form>
+  );
+}
 
 // <form onSubmit={sendEmail}>
 // <div>
