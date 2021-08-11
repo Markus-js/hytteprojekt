a224b118dd2a71988925442e1e92f34d751063dd
Markus-js <markusbackhausen@outlook.com>
2021-08-11 11:17:32 +0200

    g

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -28,18 +28,15 @@ export default function Form({ formData }) {
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
       <form className="contact-form" onSubmit={sendEmail}>
-        <input type="hidden" name="contact_number" />
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="name" />
         <label>Email</label>
         <input type="email" name="user_email" />
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
       </form>
-    </form>
   );
 }
 
